ce38eb3fba8a8ece73dba853c97aa5e26e52ace4
bansii18 <bansi84451@gmail.com>
2025-06-13 05:20:22 +0530

    Index ROute

--- Rest_class/index.js
+++ Rest_class/index.js
@@ -3,11 +3,28 @@ const app = express();
 const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname ,"views" ) );
-app.set(express.static(path.join(__dirname,"public") ) ) ;
+app.use(express.static(path.join(__dirname,"public") ) ) ;
 const port = 8080;
 app.listen(port,()=>{
     console.log(`app is runningon port ${port}`);
 });
-app.get("/",(req,res)=>{
-    res.send("sever is well responsing");
+
+let posts = [
+    {
+        username : "apna college",
+        content : "I love Coding",
+    },
+    {
+        username : "Vraj patel",
+        content : "I love JS",
+    },
+    {
+        username : "Bansii patel",
+        content : "I love C",
+    },
+
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
 });
